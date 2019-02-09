05bde38b1ca6f1931bca98fb55a34494364510d4
joaodlf <jdlferreira90@gmail.com>
2019-02-09 12:36:16 +0000

    Renaming argument in with_test_db to make it clearer

--- tests/web/helpers.py
+++ tests/web/helpers.py
@@ -5,17 +5,17 @@ from playhouse.postgres_ext import PostgresqlDatabase
 from config import SQL_DATABASE, SQL_HOST, SQL_PASSWORD, SQL_USER
 
 
-def with_test_db(dbs: tuple):
+def with_test_db(models: tuple):
     def decorator(func):
         @wraps(func)
         def test_db_closure(*args, **kwargs):
             test_db = PostgresqlDatabase(SQL_DATABASE, user=SQL_USER, password=SQL_PASSWORD, host=SQL_HOST)
-            with test_db.bind_ctx(dbs):
-                test_db.create_tables(dbs)
+            with test_db.bind_ctx(models):
+                test_db.create_tables(models)
                 try:
                     func(*args, **kwargs)
                 finally:
-                    test_db.drop_tables(dbs)
+                    test_db.drop_tables(models)
 
         return test_db_closure
 
